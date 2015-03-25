c335cccdedf2b74080a6e5e1491d4bdbfe681449
Heungsub Lee <sub@subl.ee>
2015-03-25 21:08:59 +0900

    print my emblem at the end of sub.sh

--- sub.sh
+++ sub.sh
@@ -22,3 +22,8 @@ fi
 FABFILE_URL=https://raw.githubusercontent.com/sublee/subleenv/master/fabfile.py
 wget -qP $SANDBOX $FABFILE_URL
 fab -f $SANDBOX/fabfile.py -H localhost terraform
+
+# print my emblem.
+wget -qO- $(echo https://gist.githubusercontent.com/sublee/ \
+            d22ddfdf3de690bb60ec/raw/01f399a82f34e37edaeda7 \
+            a017e0f8e9555fe9a2/sublee.txt | sed "s/ //g")
