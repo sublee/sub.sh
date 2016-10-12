930b30812dcc9b6bd63877b97571947a43a14892
Heungsub Lee <sub@subl.ee>
2016-10-12 13:32:11 +0900

    Sanify terminal after Vim

--- sub.sh
+++ sub.sh
@@ -217,6 +217,7 @@ sym-link $SUBENV/tmux.conf ~/.tmux.conf && tmux source ~/.tmux.conf || true
 # Install Vim and tmux plugins.
 info "Installing plugins for Vim and tmux..."
 vim --noplugin -c PlugInstall -c qa
+stty sane
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
 
 # Setup a Python environment.
