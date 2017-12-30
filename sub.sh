8aac0a86775c79793230f8cbef8893fc6a743960
Heungsub Lee <sub@subl.ee>
2017-12-30 14:25:29 +0900

    Refactor fd installer

--- sub.sh
+++ sub.sh
@@ -23,9 +23,6 @@ BAK=~/.sub.sh-bak-$TIMESTAMP
 UPDATE_APT_AFTER=86400
 APT_UPDATED_AT=~/.sub.sh-apt-updated-at
 
-# Keep Debian architecture label.
-DEB_ARCHITECTURE="$(dpkg --print-architecture)"
-
 help() {
   # Print the help message for --help.
   echo "Usage: curl -sL sub.sh | bash [-s - [~/.sub.sh] OPTIONS]"
@@ -262,20 +259,20 @@ fd_version() {
 if executable fd && [[ "$(fd_version)" == "$FD_VERSION" ]]; then
   echo "Already up-to-date."
 else
+  # Remove legacy executable.
   if [[ -f /usr/local/bin/fd ]]; then
-    # Remove legacy executable.
     sudo rm -rf /usr/local/bin/fd
   fi
+  FD_DEB="$(mktemp -t fd-XXX.deb)"
   FD_DEB_URL="$(
     echo "$FD_RELEASE" | \
-    grep -e "browser_download_url.\+fd_.\+${DEB_ARCHITECTURE}\.deb\"" | \
+    grep -e "download_url.\+fd_.\+$(dpkg --print-architecture)\.deb\"" | \
     cut -d'"' -f4
   )"
-  FD_DEB_PATH="$(mktemp -t fd-XXX.deb)"
-  info "Downloading $FD_DEB_URL at $FD_DEB_PATH..."
-  curl -o "$FD_DEB_PATH" -L "$FD_DEB_URL"
-  info "Installing $FD_DEB_PATH..."
-  sudo dpkg -i "$FD_DEB_PATH"
+  info "Downloading $FD_DEB_URL at $FD_DEB..."
+  curl -L "$FD_DEB_URL" -o "$FD_DEB"
+  info "Installing $FD_DEB..."
+  sudo dpkg -i "$FD_DEB"
   echo "Installed at $(which fd)."
 fi
 
