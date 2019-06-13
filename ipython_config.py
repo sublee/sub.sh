9fc45c6c84b79ea9ea110fb8822174995f81fbc4
Heungsub Hans Lee <hans.lee@kakaobrain.com>
2019-06-13 17:49:57 +0900

    Specify IPython prompt colors

--- ipython_config.py
+++ ipython_config.py
@@ -1,20 +1,40 @@
 # Use only ANSI colors.
+import pygments
 from pygments.style import Style
 from pygments.token import Token
+
+
 class MyStyle(Style):
     styles = {
-            Token.Comment:             '#ansidarkblue',
-            Token.Keyword:             '#ansibrown',
-            Token.Keyword.Constant:    '#ansiteal',
-            Token.Keyword.Namespace:   '#ansipurple',
-            Token.Name.Builtin:        '#ansiteal',
-            Token.Name.Builtin.Pseudo: 'noinherit',
-            Token.Name.Class:          '#ansiteal',
-            Token.Name.Function:       '#ansiteal',
-            Token.Name.Exception:      '#ansidarkgreen',
-            Token.Name.Decorator:      '#ansiteal',
-            Token.Operator.Word:       '#ansibrown',
-            Token.String:              '#ansidarkred',
-            Token.Number:              '#ansidarkred',
+        Token.Comment:             'ansiblue',
+        Token.Keyword:             'ansiyellow',
+        Token.Keyword.Constant:    'ansicyan',
+        Token.Keyword.Namespace:   'ansimagenta',
+        Token.Name.Builtin:        'ansicyan',
+        Token.Name.Builtin.Pseudo: 'noinherit',
+        Token.Name.Class:          'ansicyan',
+        Token.Name.Function:       'ansicyan',
+        Token.Name.Exception:      'ansigreen',
+        Token.Name.Decorator:      'ansicyan',
+        Token.Operator.Word:       'ansiyellow',
+        Token.String:              'ansired',
+        Token.Number:              'ansired',
     }
-c.TerminalInteractiveShell.highlighting_style = MyStyle
+
+
+my_style_overrides = {
+    Token.Prompt:       'ansigreen',
+    Token.PromptNum:    'ansibrightgreen bold',
+    Token.OutPrompt:    'ansired',
+    Token.OutPromptNum: 'ansibrightred bold',
+}
+
+
+pygments_version_info = tuple(map(int, pygments.__version__.split('.')))
+if pygments_version_info < (2, 4):
+    print()
+    print('sub.sh: upgrade pygments at least 2.4')
+    print()
+else:
+    c.TerminalInteractiveShell.highlighting_style = MyStyle
+    c.TerminalInteractiveShell.highlighting_style_overrides = my_style_overrides
