a17cec11285be2e3211e96f1e8742813852a3561
Heungsub Lee <sub@subl.ee>
2019-06-16 16:31:35 +0900

    Fix IPython error with Pygments<2.4

--- ipython_config.py
+++ ipython_config.py
@@ -4,37 +4,41 @@ from pygments.style import Style
 from pygments.token import Token
 
 
-class MyStyle(Style):
-    styles = {
-        Token.Comment:             'ansiblue',
-        Token.Keyword:             'ansiyellow',
-        Token.Keyword.Constant:    'ansicyan',
-        Token.Keyword.Namespace:   'ansimagenta',
-        Token.Name.Builtin:        'ansicyan',
-        Token.Name.Builtin.Pseudo: 'noinherit',
-        Token.Name.Class:          'ansicyan',
-        Token.Name.Function:       'ansicyan',
-        Token.Name.Exception:      'ansigreen',
-        Token.Name.Decorator:      'ansicyan',
-        Token.Operator.Word:       'ansiyellow',
-        Token.String:              'ansired',
-        Token.Number:              'ansired',
+def config():
+    class ANSI(Style):
+        styles = {
+            Token.Comment:             'ansiblue',
+            Token.Keyword:             'ansiyellow',
+            Token.Keyword.Constant:    'ansicyan',
+            Token.Keyword.Namespace:   'ansimagenta',
+            Token.Name.Builtin:        'ansicyan',
+            Token.Name.Builtin.Pseudo: 'noinherit',
+            Token.Name.Class:          'ansicyan',
+            Token.Name.Function:       'ansicyan',
+            Token.Name.Exception:      'ansigreen',
+            Token.Name.Decorator:      'ansicyan',
+            Token.Operator.Word:       'ansiyellow',
+            Token.String:              'ansired',
+            Token.Number:              'ansired',
+        }
+
+    ansi_prompt_styles = {
+        Token.Prompt:       'ansigreen',
+        Token.PromptNum:    'ansibrightgreen bold',
+        Token.OutPrompt:    'ansired',
+        Token.OutPromptNum: 'ansibrightred bold',
     }
 
-
-my_style_overrides = {
-    Token.Prompt:       'ansigreen',
-    Token.PromptNum:    'ansibrightgreen bold',
-    Token.OutPrompt:    'ansired',
-    Token.OutPromptNum: 'ansibrightred bold',
-}
+    T = c.TerminalInteractiveShell  # noqa
+    T.highlighting_style = ANSI
+    T.highlighting_style_overrides = ansi_prompt_styles
 
 
+# ANSI color names have been changed since Pygments-2.4.
 pygments_version_info = tuple(map(int, pygments.__version__.split('.')))
 if pygments_version_info < (2, 4):
     print()
-    print('sub.sh: upgrade pygments at least 2.4')
+    print('sub.sh: upgrade Pygments at least 2.4 to apply style')
     print()
 else:
-    c.TerminalInteractiveShell.highlighting_style = MyStyle
-    c.TerminalInteractiveShell.highlighting_style_overrides = my_style_overrides
+    config()
